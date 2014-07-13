3f2644d891ca2d611015c30d76dc587a86afb01c
kdepp <kdepp.cd@gmail.com>
2014-07-14 00:00:51 +0800

    opt: update install shell

--- install.sh
+++ install.sh
@@ -4,28 +4,50 @@ CWD=`pwd`
 DIR=`dirname $0`
 MSG_PREFIX="Install:"
 
+function echo_block () {
+	echo -e "\n================================\n"
+}
+
+function echo_install () {
+	echo "$MSG_PREFIX $1"
+}
+
+
 # copy .vimrc to ~
 if [[ -f $DIR/.vimrc ]]; then
 	if [[ -f ~/.vimrc ]]; then
 		mv ~/.vimrc ~/.vimrc_backup && \
-		echo "$MSG_PREFIX your old .vimrc moved to ~/.vimrc_backup"
+		echo_install "your old .vimrc moved to ~/.vimrc_backup"
 	fi
 	cp $DIR/.vimrc ~ && \
-	echo "$MSG_PREFIX .vimrc copied to ~"
+	echo_install ".vimrc copied to ~"
 else
-	echo "$MSG_PREFIX cannot find .vimrc in repository."
+	echo_install "cannot find .vimrc in repository."
 fi
 
+echo_block
+
 # set up pathogen
-echo "$MSG_PREFIX installing Pathogen..."
+echo_install " installing Pathogen..."
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim && \
-echo "$MSG_PREFIX Pathogen installed."
+echo_install " Pathogen installed."
 
-# set up Trinity
-echo "$MSG_PREFIX installing Trinity..."
-cd ~/.vim/bundle && \
-git clone https://github.com/wesleyche/Trinity && \
-echo "$MSG_PREFIX Trinity installed."
+echo_block
+
+# set up plugins to be installed by pathogen
+if [[ -f $DIR/pathogen_plugins.txt ]]; then
+	while read LINE; do
+		NAME_WITH_GIT=${LINE##*/}
+		NAME_WITHOUT_GIT=${NAME_WITH_GIT%.*}
+		echo_install "installing $NAME_WITHOUT_GIT..."
+		cd ~/.vim/bundle && \
+		git clone $LINE && \
+		echo_install "$NAME_WITHOUT_GIT installed."
+		echo_block
+	done < $DIR/pathogen_plugins.txt
+else
+	echo_install " no pathogen_plugins.txt file found."
+fi
 
 cd $CWD

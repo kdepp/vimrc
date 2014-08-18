8cd4ebe16f09a1015311e7ccf7e9a8cc7c3018d8
KDepp <kdepp.cd@gmail.com>
2014-08-18 21:47:11 +0800

    opt: path variables

--- install.sh
+++ install.sh
@@ -2,27 +2,36 @@
 
 CWD=`pwd`
 DIR=`dirname $0`
-MSG_PREFIX="Install:"
+PLUGIN_CONFIG_DIR="$DIR/plugin_config"
+HOME_DIR="~"
+BUNDLE_DIR="$HOME_DIR/.vim/bundle"
+AUTOLOAD_DIR="$HOME_DIR/.vim/autoload"
+INSTALL_MSG_PREFIX="Install:"
+CONFIG_MSG_PREFIX="Config:"
 
 function echo_block () {
 	echo -e "\n================================\n"
 }
 
 function echo_install () {
-	echo "$MSG_PREFIX $1"
+	echo "$INSTALL_MSG_PREFIX $1"
+}
+
+function echo_config () {
+	echo "$CONFIG_MSG_PREFIX $1"
 }
 
 echo_block
 
 # copy .vimrc to ~
 if [[ -f $DIR/.vimrc ]]; then
-	if [[ -f ~/.vimrc ]]; then
-		mv ~/.vimrc ~/.vimrc_backup && \
-		echo_install "your old .vimrc moved to ~/.vimrc_backup"
+	if [[ -f $HOME_DIR/.vimrc ]]; then
+		mv $HOME_DIR/.vimrc $HOME_DIR/.vimrc_backup && \
+		echo_install "your old .vimrc moved to $HOME_DIR/.vimrc_backup"
 		echo_block
 	fi
-	cp $DIR/.vimrc ~ && \
-	echo_install ".vimrc copied to ~"
+	cp $DIR/.vimrc $HOME_DIR && \
+	echo_install ".vimrc copied to $HOME_DIR"
 else
 	echo_install "cannot find .vimrc in repository."
 fi
@@ -30,10 +39,10 @@ fi
 echo_block
 
 # set up pathogen
-if [[ ! -f ~/.vim/autoload/pathogen.vim ]]; then
+if [[ ! -f $AUTOLOAD_DIR/pathogen.vim ]]; then
 	echo_install " installing Pathogen..."
-	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim && \
+	mkdir -p $AUTOLOAD_DIR $BUNDLE_DIR && \
+	curl -LSso $AUTOLOAD_DIR/pathogen.vim https://tpo.pe/pathogen.vim && \
 	echo_install " Pathogen installed."
 	echo_block
 fi
@@ -43,9 +52,9 @@ if [[ -f $DIR/pathogen_plugins.txt ]]; then
 	while read LINE; do
 		NAME_WITH_GIT=${LINE##*/}
 		NAME_WITHOUT_GIT=${NAME_WITH_GIT%.*}
-		if [[ ! -d ~/.vim/bundle/$NAME_WITHOUT_GIT ]]; then
+		if [[ ! -d $BUNDLE_DIR/$NAME_WITHOUT_GIT ]]; then
 			echo_install "installing $NAME_WITHOUT_GIT..."
-			cd ~/.vim/bundle && \
+			cd $BUNDLE_DIR && \
 			git clone $LINE && \
 			echo_install "$NAME_WITHOUT_GIT installed."
 			echo_block
